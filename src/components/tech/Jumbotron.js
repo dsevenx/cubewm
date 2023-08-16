d1aa2655bce4659a305292a96c432df178cf6501
dsevenx <50168945+dsevenx@users.noreply.github.com>
2023-08-16 05:50:38 +0200

    Jumbo korrigiert

--- src/components/tech/Jumbotron.js
+++ src/components/tech/Jumbotron.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
-import styled from "styled-components";
-import CUBE_ERXLEBEN from "../../assets/CUBE_ERXLEBEN.png";
+import "./Jumbotron.css";
 
 export class Jumbotron extends Component {
   constructor(props) {
@@ -12,48 +11,20 @@ export class Jumbotron extends Component {
   }
 
   render() {
-    const Styles = styled.div`
-      .jumbo {
-        background: url(${CUBE_ERXLEBEN}) no-repeat;
-        background-size: cover;
-        color: #ccc;
-        height: 300px;
-        position: relativ;
-        z-index: -2;
-      }
-
-      .overlay {
-        background-color: #ffffff;
-        opacity: 0.6;
-        position: relativ;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        z-index: -1;
-      }
-
-      #box1 {
-        padding-top: 7px;
-      }
-    `;
-
     return (
       <div>
-        <Styles>
-          <div className="jumbotron text-white jumbotron-image shadow">
-            <div className="jumbo">
-              <div className="overlay"></div>
-              <Container>
-                <h1 align="center">
-                  <font color="black">
-                    <div id="box1">Cube-Wohnmobile</div>
-                  </font>
-                </h1>
-              </Container>
-            </div>
+        <div className="jumbotron text-white jumbotron-image shadow">
+          <div className="jumbo">
+            <div className="overlay"></div>
+            <Container>
+              <h1 align="center">
+                <font color="black">
+                  <div id="box1">Cube-Wohnmobile</div>
+                </font>
+              </h1>
+            </Container>
           </div>
-        </Styles>
+        </div>
       </div>
     );
   }
